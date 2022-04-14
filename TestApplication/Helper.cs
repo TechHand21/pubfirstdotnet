5d87312ecd7fdc8940c9ea8beafe8945586ef6cf
Gurpreet Singh <gurpreet.singh@technossus.com>
2022-04-14 14:07:07 +0530

    Helper file added with one property

---
+++ TestApplication/Helper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    public class Helper
+    {
+        public int UserID { get; set; }
+    }
+}
